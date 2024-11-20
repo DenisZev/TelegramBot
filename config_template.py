fa84997dde003b7a2596db52c9faf067a35df1b4
DessaZev <43928077+DenisZev@users.noreply.github.com>
2024-11-20 15:36:55 +0700

    Add files via upload

---
+++ config_template.py
@@ -0,0 +1,13 @@
+"""
+Template configuration file for Telegram Bot.
+Replace the placeholders with your actual values and save as config.py.
+"""
+
+API_KEY = ""  # Your API key for Wildberries
+API_KEY_CONTENT = ""  # Your API key for content and prices
+API_KEY_STATISTICS = ""  # Your API key for statistics
+BASE_URL = "https://marketplace-api.wildberries.ru/api/v3"  # Wildberries API base URL
+CONTENT_URL = "https://content-api.wildberries.ru/content/v2/get/cards"
+BOT_KEY = ""  # Telegram bot key
+CHAT_ID = ""  # Telegram chat ID
+CHECK_INTERVAL = 360  # Interval to check for new orders (seconds)
